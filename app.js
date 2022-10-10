9aec663f3b1e8897ef69681f5cd0efa36001a169
Joe <joefuller042@gmail.com>
2022-10-10 13:37:39 +0100

    implement GET /api/reviews/:review_id

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require("express");
 
-const { getCategories } = require("./controllers/games.controllers");
+const {
+  getCategories,
+  getReviewById,
+} = require("./controllers/games.controllers");
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
+app.get("/api/reviews/:review_id", getReviewById);
+
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Not Found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  }
 });
 
 module.exports = app;
--- controllers/games.controllers.js
+++ controllers/games.controllers.js
@@ -1,7 +1,30 @@
-const { selectCategories } = require("../models/games.models");
+const {
+  selectCategories,
+  selectReviewById,
+} = require("../models/games.models");
 
 exports.getCategories = (req, res, next) => {
   selectCategories().then((categories) => {
     res.status(200).send({ categories });
   });
+};
+
+exports.getReviewById = (req, res, next) => {
+  const review_id = req.params.review_id;
+
+  if (!review_id) {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    selectReviewById(review_id)
+      .then((review) => {
+        if (review) {
+          res.status(200).send({ review });
+        } else {
+          res.status(404).send({ msg: "Not Found" });
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 };
